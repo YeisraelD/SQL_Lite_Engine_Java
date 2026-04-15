8cd9aa52394004727a63fc519ea693bab1ab0c7d
YeisraelD <Yeisraeld@gmail.com>
2026-04-15 21:59:22 +0300

    implementing the file system abstraction layer.

---
+++ src/neondb/storage/Page.java
@@ -0,0 +1,15 @@
+package neondb.storage;
+
+public class Page {
+    public static final int PAGE_SIZE = 4096;
+    private int pageNumber;
+    private byte[] data;
+
+    public Page(int pageNumber, byte[] data) {
+        this.pageNumber = pageNumber;
+        this.data = data;
+    }
+
+    public int getPageNumber() { return pageNumber; }
+    public byte[] getData() { return data; }
+}
---
+++ src/neondb/storage/Pager.java
@@ -0,0 +1,40 @@
+package neondb.storage;
+
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.io.IOException;
+
+public class Pager {
+    private RandomAccessFile file;
+    private long fileLength;
+    private int numPages;
+
+    public Pager(String filename) throws IOException {
+        File f = new File(filename);
+        file = new RandomAccessFile(f, "rw");
+        fileLength = file.length();
+        numPages = (int) (fileLength / Page.PAGE_SIZE);
+        if (fileLength % Page.PAGE_SIZE != 0) {
+            throw new IOException("Db file is not an integer number of pages. Corrupt file.");
+        }
+    }
+
+    public Page getPage(int pageNumber) throws IOException {
+        if (pageNumber > numPages) {
+            return null; // out of bounds
+        }
+        file.seek((long) pageNumber * Page.PAGE_SIZE);
+        byte[] data = new byte[Page.PAGE_SIZE];
+        file.read(data);
+        return new Page(pageNumber, data);
+    }
+    
+    public void writePage(Page page) throws IOException {
+        file.seek((long) page.getPageNumber() * Page.PAGE_SIZE);
+        file.write(page.getData());
+    }
+
+    public void close() throws IOException {
+        file.close();
+    }
+}
