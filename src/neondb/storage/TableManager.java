7b1950a3139a0ec86e310603a1e028cfb44f5690
YeisraelD <Yeisraeld@gmail.com>
2026-04-15 22:01:52 +0300

    Commit 4: Table Persistence

---
+++ src/neondb/storage/TableManager.java
@@ -0,0 +1,43 @@
+package neondb.storage;
+
+import java.io.IOException;
+
+public class TableManager {
+    private Table table;
+    private Pager pager;
+    private int rowsPerPage;
+
+    public TableManager(Table table, String dbFile) throws IOException {
+        this.table = table;
+        this.pager = new Pager(dbFile);
+        // Simplified row size calculation
+        this.rowsPerPage = Page.PAGE_SIZE / 256; 
+    }
+
+    public void insert(Row row) throws IOException {
+        // In memory addition
+        table.insertRow(row);
+        
+        // Persistence logic (simplified)
+        int rowNum = table.getRows().size();
+        int pageNum = rowNum / rowsPerPage;
+        
+        Page page = pager.getPage(pageNum);
+        if(page == null) {
+            page = new Page(pageNum, new byte[Page.PAGE_SIZE]);
+        }
+        
+        // Simplified: serializing row into page's byte array would go here
+        
+        // Write the updated page back to disk
+        pager.writePage(page);
+    }
+    
+    public Table getTable() {
+        return table;
+    }
+    
+    public void close() throws IOException {
+        pager.close();
+    }
+}
